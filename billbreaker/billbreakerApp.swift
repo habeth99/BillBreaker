2488e761408c7b38b688c2372328caa545027e14
Gary Miller <garymillerr17@gmail.com>
2024-03-22 22:15:41 -0400

    Initial Commit

---
+++ billbreaker/billbreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  billbreakerApp.swift
+//  billbreaker
+//
+//  Created by Nick Habeth on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct billbreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
